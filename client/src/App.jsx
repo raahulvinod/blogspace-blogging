05a154cf8165bd5d942b025fc84c992c7b1ffab3
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-22 15:08:11 +0530

    feat(client): add navbar with search input

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import Navbar from './components/Navbar';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div className="w-full h-screen text-blue-600 flex justify-center items-center text-4xl">
-      Hello world
-    </div>
-  );
-}
+const App = () => {
+  return <Navbar />;
+};
 
 export default App;
