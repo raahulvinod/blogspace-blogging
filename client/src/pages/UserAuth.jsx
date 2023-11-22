605a96780515ce8955bfdb4d385ceda3422a6894
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-23 02:44:42 +0530

    feat(client): add form validations

--- client/src/pages/UserAuth.jsx
+++ client/src/pages/UserAuth.jsx
@@ -1,73 +1,131 @@
-import React from 'react';
-import InputBox from '../components/InputBox';
-import googleIcon from '../images/google.png';
 import { Link } from 'react-router-dom';
+import { Formik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+
+import InputBox from '../components/InputBox';
 import AnimationWrapper from '../utils/animation';
+import googleIcon from '../images/google.png';
+
+const validationSchema = Yup.object({
+  fullname: Yup.string().test({
+    test: function (value) {
+      if (this.parent.type === 'sign-up') {
+        return (
+          (value && value.length >= 3) ||
+          this.createError({
+            message: 'Full name required. must be at least 3 characters',
+          })
+        );
+      }
+      return true;
+    },
+  }),
+  email: Yup.string()
+    .email('Invalid email address')
+    .required('Email is required'),
+  password: Yup.string()
+    .min(6, 'Password must be at least 6 characters')
+    .required('Password is required'),
+});
 
 const UserAuth = ({ type }) => {
+  const initialValues = {
+    fullname: type === 'sign-up' ? '' : undefined,
+    email: '',
+    password: '',
+    type: type,
+  };
+
+  const handleSubmit = (values) => {
+    // Handle form submission here
+    console.log(values);
+  };
+
   return (
     <AnimationWrapper keyValue={type}>
       <section className="h-cover flex items-center justify-center">
-        <form className="w-[80%] max-w-[400px]">
-          <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
-            {type === 'sign-in' ? 'Welcome back' : 'Join us today'}
-          </h1>
-          {type !== 'sign-in' ? (
-            <InputBox
-              name="fullname"
-              type="text"
-              placeholder="Full Name"
-              icon="fi-rr-user"
-            />
-          ) : (
-            ''
-          )}
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          <Form className="w-[80%] max-w-[400px]">
+            <h1 className="text-4xl font-gelasio capitalize text-center mb-24">
+              {type === 'sign-in' ? 'Welcome back' : 'Join us today'}
+            </h1>
+            {type !== 'sign-in' && (
+              <Field name="fullname">
+                {({ field }) => (
+                  <InputBox
+                    {...field}
+                    type="text"
+                    placeholder="Full Name"
+                    icon="fi-rr-user"
+                  />
+                )}
+              </Field>
+            )}
 
-          <InputBox
-            name="email"
-            type="email"
-            placeholder="Email"
-            icon="fi-rr-envelope"
-          />
+            <Field name="email">
+              {({ field }) => (
+                <InputBox
+                  {...field}
+                  type="email"
+                  placeholder="Email"
+                  icon="fi-rr-envelope"
+                />
+              )}
+            </Field>
 
-          <InputBox
-            name="password"
-            type="password"
-            placeholder="Password"
-            icon="fi-rr-key"
-          />
+            <Field name="password">
+              {({ field }) => (
+                <InputBox
+                  {...field}
+                  type="password"
+                  placeholder="Password"
+                  icon="fi-rr-key"
+                />
+              )}
+            </Field>
 
-          <button className="btn-dark center mt-14" type="submit">
-            {type.replace('-', ' ')}
-          </button>
+            <button className="btn-dark center mt-14" type="submit">
+              {type.replace('-', ' ')}
+            </button>
 
-          <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
-            <hr className="w-1/2 border-black" />
-            <p>or</p>
-            <hr className="w-1/2 border-black" />
-          </div>
+            <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+              <hr className="w-1/2 border-black" />
+              <p>or</p>
+              <hr className="w-1/2 border-black" />
+            </div>
 
-          <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
-            <img src={googleIcon} alt="google logo" className="w-5" />
-            continue with google
-          </button>
+            <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+              <img src={googleIcon} alt="google logo" className="w-5" />
+              continue with google
+            </button>
 
-          {type === 'sign-in' ? (
-            <p className="mt-6 text-dark-grey text-xl text-center">
-              Don't have an account ?{' '}
-              <Link to="/signup" className="underline text-black text-xl ml-1">
-                Join us today
-              </Link>
-            </p>
-          ) : (
-            <p className="mt-6 text-dark-grey text-xl text-center">
-              Already a member ?
-              <Link to="/signin" className="underline text-black text-xl ml-1">
-                Sign in here.
-              </Link>
-            </p>
-          )}
-        </form>
+            {type === 'sign-in' ? (
+              <p className="mt-6 text-dark-grey text-xl text-center">
+                Don't have an account ?{' '}
+                <Link
+                  to="/signup"
+                  className="underline text-black text-xl ml-1"
+                >
+                  Join us today
+                </Link>
+              </p>
+            ) : (
+              <p className="mt-6 text-dark-grey text-xl text-center">
+                Already a member ?
+                <Link
+                  to="/signin"
+                  className="underline text-black text-xl ml-1"
+                >
+                  Sign in here.
+                </Link>
+              </p>
+            )}
+          </Form>
+        </Formik>
       </section>
     </AnimationWrapper>
   );
