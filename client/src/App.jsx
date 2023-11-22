0266ba5f529913f5225f32941747978501970a5e
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-22 21:01:58 +0530

    feat(client): add nested routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 
 const App = () => {
-  return <Navbar />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route path="signin" element={<h1>Sign in page</h1>} />
+        <Route path="signup" element={<h1>Sign up page</h1>} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
