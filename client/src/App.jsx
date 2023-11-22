4ac0f68b40b6be425949934a5622eb6cd86ed36a
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-22 23:27:08 +0530

    feat(client): add user authentication form UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
+import UserAuth from './pages/UserAuth';
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navbar />}>
-        <Route path="signin" element={<h1>Sign in page</h1>} />
-        <Route path="signup" element={<h1>Sign up page</h1>} />
+        <Route path="signin" element={<UserAuth type="sign-in" />} />
+        <Route path="signup" element={<UserAuth type="sign-up" />} />
       </Route>
     </Routes>
   );
