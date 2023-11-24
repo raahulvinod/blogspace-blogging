ac30aca73696780752bcce1b15bcf34c714bc389
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-24 11:25:27 +0530

    fix: update yup password schema

--- client/src/pages/UserAuth.jsx
+++ client/src/pages/UserAuth.jsx
@@ -24,7 +24,15 @@ const validationSchema = Yup.object({
     .email('Invalid email address')
     .required('Email is required'),
   password: Yup.string()
-    .min(6, 'Password must be at least 6 characters')
+    .trim()
+    .min(8, 'Password must be at least 8 characters')
+    .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
+    .matches(/[A-Z]/, 'Password must contain at least one uppercase letter')
+    .matches(/\d/, 'Password must contain at least one number')
+    .matches(
+      /[!@#$%^&*(),.?":{}|<>]/,
+      'Password must contain at least one special character'
+    )
     .required('Password is required'),
 });
 
