46e694f423824bdd6e7ec75b0bec6e24ce98ee47
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-25 18:26:09 +0530

    feat(google-auth): implement google authentication

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,8 @@
 import asyncHandler from 'express-async-handler';
 import bcrypt from 'bcrypt';
+import { getAuth } from 'firebase-admin/auth';
 
+import admin from '../config/firebase.js';
 import User from '../models/user.model.js';
 import { formatDataToSend, generateUsername } from '../utils/user-utils.js';
 
@@ -42,14 +44,64 @@ export const signinUser = asyncHandler(async (req, res) => {
     return res.status(401).json({ error: 'Invalid email' });
   }
 
-  const isPasswordMatch = await bcrypt.compare(
-    password,
-    user.personal_info.password
-  );
+  if (!user.google_auth) {
+    const isPasswordMatch = await bcrypt.compare(
+      password,
+      user.personal_info.password
+    );
 
-  if (!isPasswordMatch) {
-    return res.status(401).json({ error: 'Incorrect password' });
+    if (!isPasswordMatch) {
+      return res.status(401).json({ error: 'Incorrect password' });
+    }
+
+    return res.status(200).json(formatDataToSend(user));
+  } else {
+    return res.status(403).json({
+      error: 'Account was created using google. Try logging in with google.',
+    });
   }
+});
 
-  return res.status(200).json(formatDataToSend(user));
+export const googleAuth = asyncHandler(async (req, res) => {
+  try {
+    const { access_token } = req.body;
+    const decodedUser = await getAuth().verifyIdToken(access_token);
+
+    let { email, name, picture } = decodedUser;
+    picture = picture.replace('s96-c', 's384-c');
+
+    let user = await User.findOne({
+      'personal_info.email': email,
+    }).select(
+      'personal_info.fullname personal_info.username personal_info.profile_img google_auth'
+    );
+
+    if (user) {
+      if (!user.google_auth) {
+        return res.status(403).json({
+          error:
+            'This email was signed up without Google. Please log in with a password to access this account.',
+        });
+      }
+    } else {
+      const username = await generateUsername(email);
+
+      user = new User({
+        personal_info: {
+          fullname: name,
+          email,
+          profile_img: picture,
+          username,
+        },
+        google_auth: true,
+      });
+
+      await user.save();
+    }
+
+    return res.status(200).json(formatDataToSend(user));
+  } catch (error) {
+    console.error('Error during Google authentication:', error);
+    return res.status(500).json({ error: 'Internal server error' });
+  }
 });
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import admin from 'firebase-admin';
 
 import connectDb from './config/dbConnect.js';
 import userRouter from './routes/user.route.js';
 import { errorHandler, notFound } from './middlewares/errorHandler.js';
+import serviceAccount from './.firebase/serviceAccountKey.json' assert { type: 'json' };
 
 const app = express();
 const PORT = process.env.PORT || 4000;
