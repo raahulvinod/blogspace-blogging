22c01c985e5d83822448fb1fbe00e2ca2633006d
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-27 18:02:50 +0530

    feat: create editor context

--- client/src/components/BlogEditor.jsx
+++ client/src/components/BlogEditor.jsx
@@ -1,14 +1,21 @@
 import { Link } from 'react-router-dom';
-import { useRef } from 'react';
+import { useContext } from 'react';
 import { Toaster, toast } from 'react-hot-toast';
 
 import logo from '../images/blog.png';
 import AnimationWrapper from '../utils/animation';
 import deafaultBanner from '../images/blog banner.png';
 import { uploadImage } from '../utils/aws';
+import { EditorContext } from '../pages/Editor';
 
 const BlogEditor = () => {
-  const blogBannerRef = useRef();
+  const {
+    blog,
+    blog: { title, banner, content, tags, des },
+    setBlog,
+  } = useContext(EditorContext);
+
+  console.log(blog);
 
   const handleBannerUpload = async (e) => {
     const image = e.target.files[0];
@@ -22,7 +29,8 @@ const BlogEditor = () => {
         if (url) {
           toast.dismiss(loadingToast);
           toast.success('Image uploaded successfully.');
-          blogBannerRef.current.src = url;
+
+          setBlog({ ...blog, banner: url });
         }
       } catch (error) {
         toast.dismiss(loadingToast);
@@ -42,6 +50,14 @@ const BlogEditor = () => {
 
     input.style.height = 'auto';
     input.style.height = input.scrollHeight + 'px';
+
+    setBlog({ ...blog, title: input.value });
+  };
+
+  const handleError = (e) => {
+    let img = e.target;
+
+    img.src = deafaultBanner;
   };
 
   return (
@@ -51,7 +67,7 @@ const BlogEditor = () => {
           <img src={logo} alt="logo" />
         </Link>
         <p className="max-md:hidden text-black line-clamp-1 w-fulls">
-          New Blog
+          {title.length ? title : 'New Blog'}
         </p>
 
         <div className="flex gap-4 ml-auto">
@@ -65,11 +81,7 @@ const BlogEditor = () => {
           <div className="mx-auto max-w-[900px] w-full">
             <div className="relative aspect-video hover:opacity-80 bg-white border-4 border-grey">
               <label htmlFor="uploadBanner">
-                <img
-                  ref={blogBannerRef}
-                  src={deafaultBanner}
-                  className="z-20"
-                />
+                <img src={banner} className="z-20" onError={handleError} />
                 <input
                   id="uploadBanner"
                   type="file"
@@ -86,6 +98,7 @@ const BlogEditor = () => {
               onKeyDown={handleTitleKeyDown}
               onChange={handleTitleChange}
             ></textarea>
+            <hr className="w-full opacity-10 my-5" />
           </div>
         </section>
       </AnimationWrapper>
--- client/src/pages/Editor.jsx
+++ client/src/pages/Editor.jsx
@@ -1,19 +1,37 @@
-import { useContext, useState } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { UserContext } from '../App';
 import { Navigate } from 'react-router-dom';
 import BlogEditor from '../components/BlogEditor';
 import PublishForm from '../components/PublishForm';
 
+const blogStructure = {
+  title: '',
+  banner: '',
+  content: [],
+  tags: [],
+  des: '',
+  author: { personal_info: {} },
+};
+
+export const EditorContext = createContext({});
+
 const Editor = () => {
+  const [blog, setBlog] = useState(blogStructure);
   const [editorState, setEditorState] = useState('editor');
 
   const { userAuth: { access_token } = {} } = useContext(UserContext);
-  return access_token === null ? (
-    <Navigate to="/signup" />
-  ) : editorState === 'editor' ? (
-    <BlogEditor />
-  ) : (
-    <PublishForm />
+  return (
+    <EditorContext.Provider
+      value={{ blog, setBlog, editorState, setEditorState }}
+    >
+      {access_token === null ? (
+        <Navigate to="/signup" />
+      ) : editorState === 'editor' ? (
+        <BlogEditor />
+      ) : (
+        <PublishForm />
+      )}
+    </EditorContext.Provider>
   );
 };
 
