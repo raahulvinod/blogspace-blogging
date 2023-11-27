34d1af339083a9c36eac2210bcab49e461bbdfd1
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-11-27 17:14:10 +0530

    feat: customize blog title input

--- client/src/components/BlogEditor.jsx
+++ client/src/components/BlogEditor.jsx
@@ -31,6 +31,19 @@ const BlogEditor = () => {
     }
   };
 
+  const handleTitleKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      e.preventDefault();
+    }
+  };
+
+  const handleTitleChange = (e) => {
+    let input = e.target;
+
+    input.style.height = 'auto';
+    input.style.height = input.scrollHeight + 'px';
+  };
+
   return (
     <>
       <nav className="navbar">
@@ -66,6 +79,13 @@ const BlogEditor = () => {
                 />
               </label>
             </div>
+
+            <textarea
+              placeholder="Blog title"
+              className="text-4xl font-medium w-full h-20 outline-none resize-none mt-10 leading-tight placeholder:opacity-40"
+              onKeyDown={handleTitleKeyDown}
+              onChange={handleTitleChange}
+            ></textarea>
           </div>
         </section>
       </AnimationWrapper>
