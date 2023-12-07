f72f0bc9cc30de9519b8f938547b29998bf5322f
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2023-12-07 15:34:04 +0530

    feat(blog): add interaction UI

--- client/src/pages/Blog.jsx
+++ client/src/pages/Blog.jsx
@@ -1,6 +1,7 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
+
 import AnimationWrapper from '../utils/animation';
 import { Loader } from '../components/Loader';
 import { getDay } from '../utils/date';
@@ -15,6 +16,8 @@ export const blogStructure = {
   banner: '',
   publishedAt: '',
 };
+
+export const BlogContext = createContext({});
 
 const Blog = () => {
   const { blogId } = useParams();
@@ -59,30 +62,32 @@ const Blog = () => {
       {loading ? (
         <Loader />
       ) : (
-        <div className="max-w-[900px] center py-10 max-lg:px-5vw">
-          <img src={banner} className="aspect-video" />
+        <BlogContext.Provider value={{ blog, setBlog }}>
+          <div className="max-w-[900px] center py-10 max-lg:px-5vw">
+            <img src={banner} className="aspect-video" />
 
-          <div className="mt-12">
-            <h2>{title}</h2>
+            <div className="mt-12">
+              <h2>{title}</h2>
 
-            <div className="flex max-sm:flex-col justify-between my-8">
-              <div className="flex gap-5 items-start">
-                <img src={profile_img} className="w-12 h-12 rounded-full" />
-                <p className="capitalize">
-                  {fullname} <br />@
-                  <Link to={`/user/${author_username}`} className="underline">
-                    {author_username}
-                  </Link>
+              <div className="flex max-sm:flex-col justify-between my-8">
+                <div className="flex gap-5 items-start">
+                  <img src={profile_img} className="w-12 h-12 rounded-full" />
+                  <p className="capitalize">
+                    {fullname} <br />@
+                    <Link to={`/user/${author_username}`} className="underline">
+                      {author_username}
+                    </Link>
+                  </p>
+                </div>
+                <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">
+                  Published on {getDay(publishedAt)}
                 </p>
               </div>
-              <p className="text-dark-grey opacity-75 max-sm:mt-6 max-sm:ml-12 max-sm:pl-5">
-                Published on {getDay(publishedAt)}
-              </p>
-            </div>
 
-            <BlogInteraction />
+              <BlogInteraction />
+            </div>
           </div>
-        </div>
+        </BlogContext.Provider>
       )}
     </AnimationWrapper>
   );
