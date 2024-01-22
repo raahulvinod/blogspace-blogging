6a4cffb3424b51b1f497fa527e69c9c58dd9d9fe
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-01-22 17:05:43 +0530

    feat(manage-blog): get user blogs and drafts

--- client/src/pages/ManageBlogs.jsx
+++ client/src/pages/ManageBlogs.jsx
@@ -1,5 +1,91 @@
+import axios from 'axios';
+import { useContext, useEffect, useState } from 'react';
+
+import { UserContext } from '../App';
+import { filterPagination } from '../utils/filterPagination';
+import { Toaster } from 'react-hot-toast';
+
 const ManageBlogs = () => {
-  return <div>BlogManagement</div>;
+  const [blogs, setBlogs] = useState(null);
+  const [drafts, setDrafts] = useState(null);
+  const [query, setQuery] = useState('');
+
+  const { userAuth: { access_token } = {} } = useContext(UserContext);
+
+  const getBlogs = async ({ page, draft, deletedDocCount = 0 }) => {
+    try {
+      const { data } = await axios.post(
+        import.meta.env.VITE_SERVER_DOMAIN + '/user-written-blogs',
+        { page, draft, query, deletedDocCount },
+        {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        }
+      );
+
+      const formattedData = await filterPagination({
+        state: draft ? drafts : blogs,
+        data: data.blogs,
+        page,
+        user: access_token,
+        countRoute: '/user-written-blogs-count',
+        data_to_send: { draft, query },
+      });
+
+      console.log(formattedData);
+
+      if (draft) setDrafts(formattedData);
+      else setBlogs(formattedData);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const searchQuery = e.target.value;
+
+    setQuery(searchQuery);
+
+    if (e.keyCode === 13 && searchQuery.length) {
+      setBlogs(null);
+      setDrafts(null);
+    }
+  };
+
+  const handleChange = (e) => {
+    if (!e.target.value.length) {
+      setQuery('');
+      setBlogs(null);
+      setDrafts(null);
+    }
+  };
+
+  useEffect(() => {
+    if (access_token) {
+      if (blogs === null) getBlogs({ page: 1, draft: false });
+      if (drafts === null) getBlogs({ page: 1, draft: true });
+    }
+  }, [access_token, blogs, drafts, query]);
+
+  return (
+    <>
+      <h1 className="max-md:hidden">Manage Blogs</h1>
+
+      <Toaster />
+
+      <div className="relative max-md:mt-5 md:mt-8 mb-10">
+        <input
+          type="search"
+          className="w-full bg-grey p-4 pl-12 pr-6 rounded-full placeholder:text-dark-grey"
+          placeholder="Search blogs"
+          onChange={handleChange}
+          onKeyDown={handleSearch}
+        />
+        <i className="fi fi-rr-search absolute right-[10%] md:pointer-events-none md:left-5 top-1/2 -translate-y-1/2 text-xl text-dark-grey"></i>
+      </div>
+    </>
+  );
 };
 
 export default ManageBlogs;
