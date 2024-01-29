d59e901d83092351c850e93bbd68a219c8764bf8
Rahul vinod <120365941+raahulvinod@users.noreply.github.com>
2024-01-29 16:31:50 +0530

    chore(deployment): updated deployment configuration for backend

--- server/config/firebase.js
+++ server/config/firebase.js
@@ -1,6 +1,5 @@
 import admin from 'firebase-admin';
-import serviceAccount from '../.firebase/serviceAccountKey.json' assert { type: 'json' };
-// import serviceAccount from '../serviceAccountKey.json' assert { type: 'json' };
+import serviceAccount from '../serviceAccountKey.json' assert { type: 'json' };
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
